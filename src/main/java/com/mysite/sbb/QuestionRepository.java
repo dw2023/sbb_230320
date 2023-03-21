5a6d33deaee094be67eed952fa64e27da1d5b085
dw2023 <dwkim126@gmail.com>
2023-03-21 13:50:06 +0900

    각 테스트케이스의 독립성 확보(수정2)

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -19,9 +19,4 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String subject, String content);
 
     List<Question> findBySubjectLike(String subject);
-
-    // 만약 아래 쿼리가 UPDATE, DELETE, INSERT 라면 @Modifying을 붙여야 한다.
-    // nativeQuery = true 여야 MySQL 쿼리문법 사용 가능
-    @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
-    void clearAutoIncrement();
 }
